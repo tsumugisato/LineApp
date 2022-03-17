20ea8206e44d8478e36316c5a3c0ebfcefcf4e51
satoutsumugi <tsumugisato0@gmail.com>
2022-03-17 22:45:56 +0900

    chatlistviewcontroller

---
+++ LineApp/ChatListViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ChatListViewController.swift
+//  LineApp
+//
+//  Created by 佐藤紬 on 2022/03/17.
+//
+
+import UIKit
+
+class ChatListViewController:UIViewController{
+    
+    private let cellId = "cellId"
+    
+    @IBOutlet weak var chatListTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        chatListTableView.delegate = self
+        chatListTableView.dataSource = self
+    }
+}
+extension ChatListViewController:UITableViewDelegate,UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = chatListTableView.dequeueReusableCell(withIdentifier: cellId,for:indexPath)
+        return cell
+    }
+}
+
+class ChatListTableViewCell:UITableViewCell{
+    @IBOutlet weak var partnerLabel:UILabel!
+    @IBOutlet weak var letestMessageLabel:UILabel!
+    @IBOutlet weak var userImageView:UIImageView!
+    @IBOutlet weak var dateLabel:UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        userImageView.layer.cornerRadius = 35
+    }
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
